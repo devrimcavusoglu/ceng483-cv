02533987b409c791c181d020d7724eaeda30e15b
devrimcavusoglu <devrimcavusoglu@gmail.com>
2023-01-15 20:33:46 +0300

    Report is added.

--- the3/submission.sh
+++ the3/submission.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-tar --exclude='checkpoints' --exclude='ceng483-f22-hw3-dataset' --exclude='*.pdf' --exclude="*.sh" -czf 2010023_the3.tar.gz .
+tar --exclude='checkpoints' --exclude='ceng483-f22-hw3-dataset' --exclude='template.pdf' --exclude='the3.pdf' --exclude="*.sh" -czf 2010023_the3.tar.gz .
 sleep 2
